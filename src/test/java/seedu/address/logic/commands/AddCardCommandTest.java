a13e44bcc91a7a7d393d1617a817cf56e7b63fa4
pukipuki <dfwong5566@gmail.com>
2018-03-14 09:16:36 +0800

    fix style checks

--- src/test/java/seedu/address/logic/commands/AddCardCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCardCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Rule;
 import org.junit.Test;
