d4c4f22addd9a75d25ff3ba427c18d2f45d0556e
yong-jie <viorama@gmail.com>
2018-03-28 02:18:45 +0800

    Add select card command with tests (exclude system tests)

---
+++ src/test/java/seedu/address/logic/parser/SelectCardCommandParserTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+import seedu.address.logic.commands.SelectCardCommand;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CARD;
+
+/**
+ * Tests the parsing functionality of {@code SelectCardCommandParser}
+ */
+public class SelectCardCommandParserTest {
+
+    private SelectCardCommandParser parser = new SelectCardCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSelectCardCommand() {
+        assertParseSuccess(parser, "1", new SelectCardCommand(INDEX_FIRST_CARD));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCardCommand.MESSAGE_USAGE));
+    }
+}
