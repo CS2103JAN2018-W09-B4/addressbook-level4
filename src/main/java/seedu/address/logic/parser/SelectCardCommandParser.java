beae77edce8d8b24acfd7f21545fbb36815ee926
yong-jie <viorama@gmail.com>
2018-03-29 20:18:44 +0800

    More collation

--- src/main/java/seedu/address/logic/parser/SelectCardCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SelectCardCommandParser.java
@@ -7,6 +7,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.SelectCardCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+//@@author yong-jie
 /**
  * Parses input arguments and creates a new SelectCardCommand object
  */
