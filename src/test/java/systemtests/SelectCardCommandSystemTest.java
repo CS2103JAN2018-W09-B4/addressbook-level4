8b1d29df3d096cfda9d0ea2a57bf7a5aa977e8ce
yong-jie <viorama@gmail.com>
2018-03-30 21:09:53 +0800

    Fix test case from SelectCardCommandSystemTest

--- src/test/java/systemtests/SelectCardCommandSystemTest.java
+++ src/test/java/systemtests/SelectCardCommandSystemTest.java
@@ -95,11 +95,10 @@ public class SelectCardCommandSystemTest extends AddressBookSystemTest {
         /* Case: mixed case command word -> rejected */
         assertCommandFailure("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
 
-        // TODO: Fix this
-        /* Case: select from empty address book -> rejected */
-        // selectTag(Index.fromOneBased(1));
-        // assertCommandFailure(SelectCardCommand.COMMAND_WORD + " " + INDEX_FIRST_CARD.getOneBased(),
-        // MESSAGE_INVALID_CARD_DISPLAYED_INDEX);
+        /* Case: select from empty cardList -> rejected */
+        selectTag(Index.fromOneBased(2));
+        assertCommandFailure(SelectCardCommand.COMMAND_WORD + " " + INDEX_FIRST_CARD.getOneBased(),
+                MESSAGE_INVALID_CARD_DISPLAYED_INDEX);
     }
 
     /**
